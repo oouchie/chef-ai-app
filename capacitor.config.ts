41e64955de1ea4b1452f69bc7ceadb948854e2a9
Oouchie Bates <oouchie@gmail.com>
2026-01-12 15:55:39 -0500

    Change bundle ID to com.chefai2.app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.chefai.app',
+  appId: 'com.chefai2.app',
   appName: 'Chef AI',
   webDir: 'out',
   server: {
